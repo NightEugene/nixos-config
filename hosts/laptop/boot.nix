48844db0ac0dd54c851d0e74b5657ed351c5f9af
Eugene Todoruk <nighteugene@ya.ru>
2026-04-13 16:43:26 +0300

    Code style format

--- hosts/laptop/boot.nix
+++ hosts/laptop/boot.nix
@@ -11,7 +11,7 @@
     }
   '';
 
-  boot.kernelParams = ["resume_offset=1632256"];
+  boot.kernelParams = [ "resume_offset=1632256" ];
 
   boot.resumeDevice = "/dev/disk/by-uuid/9bdf09c9-e4d5-4b48-810f-42b1228cb040";
 }
