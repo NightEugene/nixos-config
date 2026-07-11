976c6a97de504c46e82ecceac2154d409194a449
Eugene Todoruk <nighteugene@ya.ru>
2026-07-11 14:50:17 +0300

    fix(hosts/home): reduce pointer cursor size to 16

--- hosts/home/users/nighteugene/home-configuration.nix
+++ hosts/home/users/nighteugene/home-configuration.nix
@@ -1,10 +1,13 @@
-{ flake, ... }:
+{ flake, lib, ... }:
 
 {
   imports = [
     flake.homeModules.default
     flake.homeModules.noctaliaHome
   ];
+
+  # На десктопе курсор 24 пикселя выглядит слишком крупно.
+  home.pointerCursor.size = lib.mkForce 16;
 
   # Десктоп home: сюда можно добавить пакеты, специфичные для стационарного ПК.
   # Пример: home.packages = with pkgs; [ steam ];
